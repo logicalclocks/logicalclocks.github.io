35dd94731f2adce1ab2d6ce2fec35b4471e61619
rktraz <roman.kaharlytskyi@gmail.com>
2023-02-22 13:29:17 +0100

    minifyed css and js files

--- docs/js/dropdown.js
+++ docs/js/dropdown.js
@@ -1,3 +1,1 @@
-document.getElementsByClassName("md-tabs__link")[7].style.display = "none";
-document.getElementsByClassName("md-tabs__link")[9].style.display = "none";
-
+document.getElementsByClassName("md-tabs__link")[7].style.display="none",document.getElementsByClassName("md-tabs__link")[9].style.display="none";
