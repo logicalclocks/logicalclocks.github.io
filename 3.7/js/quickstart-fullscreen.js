7b12b0c9a36bbd8fde5955f59d398bb007cdadf5
Mike <mike@docs.hopsworks.ai>
2024-04-10 15:03:21 +0000

    Deployed fdf50b9d to 3.7 with MkDocs 1.5.3 and mike 2.0.0

--- 3.7/js/quickstart-fullscreen.js
+++
@@ -1,8 +0,0 @@
-/*
-    This script removes the sidebar from the Getting Started page for the example to expand whole page on non-mobile devices.
-*/
-
-//https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent for recommended approach to test if device is mobile
-if(window.location.pathname.endsWith('hopsworks-tutorials/quickstart/') && !/Mobi|Android/i.test(navigator.userAgent)) {
-    document.getElementsByClassName("md-sidebar--primary")[0].remove()
-}
