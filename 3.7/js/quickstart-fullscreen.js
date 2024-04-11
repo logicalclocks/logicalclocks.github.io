a9f2d65fe4ee843d911595a16009929f55cf5292
Mike <mike@docs.hopsworks.ai>
2024-04-11 08:28:33 +0000

    Deployed 8bc043ec to 3.7 with MkDocs 1.5.3 and mike 2.0.0

--- 3.7/js/quickstart-fullscreen.js
+++ 3.7/js/quickstart-fullscreen.js
@@ -3,9 +3,6 @@
 */
 
 //https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent for recommended approach to test if device is mobile
-if (
-  window.location.pathname.endsWith("hopsworks-tutorials/quickstart/") &&
-  !/Mobi|Android/i.test(navigator.userAgent)
-) {
-  document.getElementsByClassName("md-sidebar--primary")[0].remove();
+if(window.location.pathname.endsWith('hopsworks-tutorials/quickstart/') && !/Mobi|Android/i.test(navigator.userAgent)) {
+    document.getElementsByClassName("md-sidebar--primary")[0].remove()
 }
