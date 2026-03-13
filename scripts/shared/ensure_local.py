6e509e1a9ae7cb381aa6bdfd13b2dbe39e0c1069
Aleksey Veresov <aleksey@veresov.pro>
2026-03-13 17:04:48 +0100

    Fix

--- scripts/shared/ensure_local.py
+++ scripts/shared/ensure_local.py
@@ -8,5 +8,9 @@ def ensure_local(path: Path) -> None:
     """Ensure the given file exists locally, copying from the repo if not."""
     local_path = path.relative_to(_DOCS_ROOT)
     if not local_path.exists():
+        for p in reversed(local_path.parents):
+            if not p.exists():
+                atexit.register(p.rmdir)
+                p.mkdir()
         atexit.register(lambda: local_path.unlink())
         local_path.write_bytes(path.read_bytes())
