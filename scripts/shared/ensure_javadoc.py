d5e39518b1f3faba575510af9592461581e61f12
Aleksey Veresov <aleksey@veresov.pro>
2026-03-16 14:56:58 +0100

    Gitignore javadoc

--- scripts/shared/ensure_javadoc.py
+++ scripts/shared/ensure_javadoc.py
@@ -7,5 +7,14 @@ def ensure_javadoc() -> None:
     """Ensure javadoc is present."""
     path = _DOCS_ROOT / "docs" / "javadoc"
     if not path.exists():
+        gipath = path.parent / ".gitignore"
+        if gipath.exists():
+            gi = gipath.read_text()
+        else:
+            gi = "/.gitignore\n"
+            atexit.register(lambda: gipath.unlink())
+        if "/" + path.name not in gi:
+            gipath.write_text(gi + f"\n/{path.name}\n")
+
         atexit.register(lambda: path.unlink())
         path.write_text("")
